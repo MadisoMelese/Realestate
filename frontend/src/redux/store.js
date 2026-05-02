f743590d14ffc614b32ba012e0f79e2b6fdb9524
Madiso Melese <madishamadiso00@gmail.com>
2026-05-02 19:54:52 +0300

    feat: register adminReducer in Redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import propertyReducer from './slices/propertySlice';
 import transactionReducer from './slices/transactionSlice';
+import adminReducer from './slices/adminSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     property: propertyReducer,
-    transaction: transactionReducer
+    transaction: transactionReducer,
+    admin: adminReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
